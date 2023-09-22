6082b14e9a5b4cf8f0b674f2e1b0de5f796ef99f
Andriymatvienko <130711488+Andriymatvienko@users.noreply.github.com>
2023-09-23 00:36:12 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/CCircle.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp2
+{
+    class CCircle
+    {
+        const int DefaultRadius = 50;
+
+        private Graphics graphics;
+        private int _radius;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value >= 200 ? 200 : value;
+                _radius = value <= 5 ? 5 : value;
+            }
+        }
+
+        public CCircle(Graphics graphics, int X, int Y)
+        {
+            this.graphics = graphics;
+            this.X = X;
+            this.Y = Y;
+            this.Radius = DefaultRadius;
+        }
+
+        public CCircle(Graphics graphics, int X, int Y, int Radius)
+        {
+            this.graphics = graphics;
+            this.X = X;
+            this.Y = Y;
+            this.Radius = Radius;
+        }
+
+        private void DrawCircle(Pen pen)
+        {
+            Rectangle rectangle = new Rectangle(X - Radius, Y + Radius, 2 * Radius, 2 * Radius);
+            graphics.ResetTransform();
+            graphics.TranslateTransform(X, Y);
+            graphics.RotateTransform(90);
+            graphics.TranslateTransform(-X, -Y);
+            graphics.DrawEllipse(pen, rectangle);
+        }
+
+        private void DrawSquare(Pen pen)
+        {
+            int squareSideLength = 2 * Radius;
+            Rectangle squareRect = new Rectangle(X - squareSideLength / 2, Y - squareSideLength / 2, squareSideLength, squareSideLength);
+            graphics.ResetTransform();
+            graphics.TranslateTransform(X, Y);
+            graphics.RotateTransform(90);
+            graphics.TranslateTransform(-X, -Y);
+            graphics.DrawRectangle(pen, squareRect);
+        }
+
+        private void DrawTriangle(Pen pen)
+        {
+            int triangleSideLength = 2 * Radius;
+            int triangleX = X + 2 * Radius;
+            Point[] originalTrianglePoints = new Point[]
+            {
+        new Point(triangleX, Y - triangleSideLength / 2),
+        new Point(triangleX + triangleSideLength / 2, Y + triangleSideLength / 2),
+        new Point(triangleX - triangleSideLength / 2, Y + triangleSideLength / 2)
+            };
+            graphics.ResetTransform();
+            graphics.TranslateTransform(triangleX, Y);
+            graphics.RotateTransform(90);
+            graphics.TranslateTransform(-triangleX, -Y);
+            graphics.DrawPolygon(pen, originalTrianglePoints);
+        }
+
+        public void Show()
+        {
+            DrawCircle(Pens.Red);
+            DrawSquare(Pens.Green);
+            DrawTriangle(Pens.Blue);
+        }
+
+        public void Hide()
+        {
+            DrawCircle(Pens.White);
+            DrawSquare(Pens.White);
+            DrawTriangle(Pens.White);
+        }
+
+        public void Expand()
+        {
+            Hide();
+            Radius++;
+            Show();
+        }
+
+        public void Expand(int dR)
+        {
+            Hide();
+            Radius = Radius + dR;
+            Show();
+        }
+
+        public void Collapse()
+        {
+            Hide();
+            Radius--;
+            Show();
+        }
+
+        public void Collapse(int dR)
+        {
+            Hide();
+            Radius = Radius - dR;
+            Show();
+        }
+
+        public void Move(int dX, int dY)
+        {
+            Hide();
+            X = X + dX;
+            Y = Y + dY;
+            Show();
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/fMain.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.pnTools = new System.Windows.Forms.Panel();
+            this.btnCollapse = new System.Windows.Forms.Button();
+            this.btnExpand = new System.Windows.Forms.Button();
+            this.btnDownFar = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnLeftFar = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnRightFar = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnUpFar = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnCreateNew = new System.Windows.Forms.Button();
+            this.cbCircles = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.BackColor = System.Drawing.Color.White;
+            this.pnMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(480, 450);
+            this.pnMain.TabIndex = 0;
+            // 
+            // pnTools
+            // 
+            this.pnTools.Controls.Add(this.btnCollapse);
+            this.pnTools.Controls.Add(this.btnExpand);
+            this.pnTools.Controls.Add(this.btnDownFar);
+            this.pnTools.Controls.Add(this.button4);
+            this.pnTools.Controls.Add(this.btnLeft);
+            this.pnTools.Controls.Add(this.btnLeftFar);
+            this.pnTools.Controls.Add(this.btnRight);
+            this.pnTools.Controls.Add(this.btnRightFar);
+            this.pnTools.Controls.Add(this.btnUp);
+            this.pnTools.Controls.Add(this.btnUpFar);
+            this.pnTools.Controls.Add(this.btnShow);
+            this.pnTools.Controls.Add(this.btnHide);
+            this.pnTools.Controls.Add(this.btnCreateNew);
+            this.pnTools.Controls.Add(this.cbCircles);
+            this.pnTools.Controls.Add(this.label1);
+            this.pnTools.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnTools.Location = new System.Drawing.Point(482, 0);
+            this.pnTools.Name = "pnTools";
+            this.pnTools.Size = new System.Drawing.Size(318, 450);
+            this.pnTools.TabIndex = 1;
+            // 
+            // btnCollapse
+            // 
+            this.btnCollapse.Location = new System.Drawing.Point(114, 329);
+            this.btnCollapse.Name = "btnCollapse";
+            this.btnCollapse.Size = new System.Drawing.Size(59, 23);
+            this.btnCollapse.TabIndex = 14;
+            this.btnCollapse.Text = "btnCollapse";
+            this.btnCollapse.UseVisualStyleBackColor = true;
+            this.btnCollapse.Click += new System.EventHandler(this.btnCollapse_Click);
+            // 
+            // btnExpand
+            // 
+            this.btnExpand.Location = new System.Drawing.Point(114, 287);
+            this.btnExpand.Name = "btnExpand";
+            this.btnExpand.Size = new System.Drawing.Size(62, 23);
+            this.btnExpand.TabIndex = 13;
+            this.btnExpand.Text = "btnExpand";
+            this.btnExpand.UseVisualStyleBackColor = true;
+            this.btnExpand.Click += new System.EventHandler(this.btnExpand_Click);
+            // 
+            // btnDownFar
+            // 
+            this.btnDownFar.Location = new System.Drawing.Point(97, 401);
+            this.btnDownFar.Name = "btnDownFar";
+            this.btnDownFar.Size = new System.Drawing.Size(89, 24);
+            this.btnDownFar.TabIndex = 12;
+            this.btnDownFar.Text = "btnDownFar";
+            this.btnDownFar.UseVisualStyleBackColor = true;
+            this.btnDownFar.Click += new System.EventHandler(this.btnDownFar_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(97, 372);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "btnDown (BT4)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(57, 276);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(41, 90);
+            this.btnLeft.TabIndex = 10;
+            this.btnLeft.Text = "btnLeft";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnLeftFar
+            // 
+            this.btnLeftFar.Location = new System.Drawing.Point(4, 276);
+            this.btnLeftFar.Name = "btnLeftFar";
+            this.btnLeftFar.Size = new System.Drawing.Size(46, 90);
+            this.btnLeftFar.TabIndex = 9;
+            this.btnLeftFar.Text = "btnLeftFar";
+            this.btnLeftFar.UseVisualStyleBackColor = true;
+            this.btnLeftFar.Click += new System.EventHandler(this.btnLeftFar_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(195, 276);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(43, 90);
+            this.btnRight.TabIndex = 8;
+            this.btnRight.Text = "btnRight";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // btnRightFar
+            // 
+            this.btnRightFar.Location = new System.Drawing.Point(244, 276);
+            this.btnRightFar.Name = "btnRightFar";
+            this.btnRightFar.Size = new System.Drawing.Size(41, 90);
+            this.btnRightFar.TabIndex = 7;
+            this.btnRightFar.Text = "btnRightFar";
+            this.btnRightFar.UseVisualStyleBackColor = true;
+            this.btnRightFar.Click += new System.EventHandler(this.btnRightFar_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(97, 242);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(89, 25);
+            this.btnUp.TabIndex = 6;
+            this.btnUp.Text = "btnUp";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnUpFar
+            // 
+            this.btnUpFar.Location = new System.Drawing.Point(97, 213);
+            this.btnUpFar.Name = "btnUpFar";
+            this.btnUpFar.Size = new System.Drawing.Size(89, 23);
+            this.btnUpFar.TabIndex = 5;
+            this.btnUpFar.Text = "btnUpFar";
+            this.btnUpFar.UseVisualStyleBackColor = true;
+            this.btnUpFar.Click += new System.EventHandler(this.btnUpFar_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(48, 161);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(190, 27);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Показати об\'єкт";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(48, 128);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(190, 27);
+            this.btnHide.TabIndex = 3;
+            this.btnHide.Text = "Приховати об\'єкт";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnCreateNew
+            // 
+            this.btnCreateNew.Location = new System.Drawing.Point(48, 97);
+            this.btnCreateNew.Name = "btnCreateNew";
+            this.btnCreateNew.Size = new System.Drawing.Size(190, 25);
+            this.btnCreateNew.TabIndex = 2;
+            this.btnCreateNew.Text = "Створити новий об\'єкт";
+            this.btnCreateNew.UseVisualStyleBackColor = true;
+            this.btnCreateNew.Click += new System.EventHandler(this.btnCreateNew_Click_1);
+            // 
+            // cbCircles
+            // 
+            this.cbCircles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCircles.FormattingEnabled = true;
+            this.cbCircles.Location = new System.Drawing.Point(48, 54);
+            this.cbCircles.Name = "cbCircles";
+            this.cbCircles.Size = new System.Drawing.Size(190, 24);
+            this.cbCircles.TabIndex = 1;
+            this.cbCircles.SelectedIndexChanged += new System.EventHandler(this.cbCircles_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Перелік об\'єктів";
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnTools);
+            this.Controls.Add(this.pnMain);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №1 Матвіоєнко ";
+            this.pnTools.ResumeLayout(false);
+            this.pnTools.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Panel pnTools;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnLeftFar;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnRightFar;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnUpFar;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnCreateNew;
+        private System.Windows.Forms.ComboBox cbCircles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCollapse;
+        private System.Windows.Forms.Button btnExpand;
+        private System.Windows.Forms.Button btnDownFar;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
